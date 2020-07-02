babb9aca299cb8005966dc8a55f0d67d8bd67550
kibuniverse <3050540005@qq.com>
2020-07-02 16:14:55 +0800

    add new tab

--- src/components/blogPage/index.js
+++ src/components/blogPage/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Mainer from './Mainer.jsx'
+import Header from '../HomePage/Header.jsx'
 
 
 const App = () => (
     <div>
-        blog page
+        <Header />
+        <Mainer />
     </div>
+    
 )
 
+
 ReactDOM.render(<App />, document.getElementById('root'))
+
