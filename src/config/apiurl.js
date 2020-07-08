dece8bb59e19c9a40fad977ab28a485997d68d75
kibuniverse <3050540005@qq.com>
2020-07-08 22:49:05 +0800

    init api配置文件

---
+++ src/config/apiurl.js
@@ -0,0 +1,8 @@
+let ipUrl = 'http://127.0.0.1:7001/default/'
+
+let servicePath = {
+    getArticleList: ipUrl + 'getArticleList',   // 获取首页文章列表接口
+    getArticleById: ipUrl + 'getArticleById'
+}
+
+export default servicePath
